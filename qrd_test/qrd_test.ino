d7cc8ed90a9eae36e934c8543f49b2702d53250b
bcorbold <bradencorbold@gmail.com>
2017-11-20 15:23:41 -0500

    Updated for 2017 comp

--- qrd_test/qrd_test.ino
+++ qrd_test/qrd_test.ino
@@ -1,17 +1,32 @@
-const unsigned int qrdPin = A1;
+const unsigned int B_R_QRD_PIN = A0;
+const unsigned int F_R_QRD_PIN = A1;
+const unsigned int F_L_QRD_PIN = A3;
+const unsigned int B_L_QRD_PIN = A2;
 
-void getQrdValue(){
-  Serial.print("QRD Value: ");
-  Serial.println(analogRead(qrdPin));
-}
 
 void setup(){
-  pinMode(qrdPin, INPUT);
+  pinMode(F_L_QRD_PIN, INPUT);
+  pinMode(F_R_QRD_PIN, INPUT);
+  pinMode(B_L_QRD_PIN, INPUT);
+  pinMode(B_R_QRD_PIN, INPUT);
 
   Serial.begin(9600);
 }
 
 void loop(){
-  getQrdValue();
+  getQrdState();
   delay(500);
+}
+
+
+void getQrdState() {
+  unsigned int f_l_val = analogRead(F_L_QRD_PIN);
+  unsigned int f_r_val = analogRead(F_R_QRD_PIN);
+  unsigned int b_l_val = analogRead(B_L_QRD_PIN);
+  unsigned int b_r_val = analogRead(B_R_QRD_PIN);
+
+  Serial.println(f_l_val);
+  Serial.println(f_r_val);
+  Serial.println(b_l_val);
+  Serial.println(b_r_val);
 }
